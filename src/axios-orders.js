3c9093a69c8111a2152d50d0fca3527d8baefc12
Azat Nasyrov <azat.nasyrov31@gmail.com>
2022-03-05 22:40:18 +0600

    I fixed the dependencies and updated the line in axios-orders.js for which I am making a request, I also added .env

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosOrders = axios.create({
-  baseURL: 'https://burger-app-e5e84-default-rtdb.firebaseio.com/'
+  baseURL: 'https://burger-app-b08f2-default-rtdb.firebaseio.com/'
 });
 
 export default axiosOrders;
